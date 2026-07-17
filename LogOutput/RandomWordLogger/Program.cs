840751c41334c1112e8f4e347b13e389ab0ceb26
pronom8 <mangros19@gmail.com>
2026-07-17 14:12:34 +0300

    1.7 External access with Ingress for the logoutput application done

--- LogOutput/RandomWordLogger/Program.cs
+++ LogOutput/RandomWordLogger/Program.cs
@@ -4,11 +4,30 @@ var randomWords = new List<string> {"jeje","koira","kissa","koroko","lehmä"};
 
 var random = new Random();
 
-while (true)
-{
-    var randomIndex = random.Next(randomWords.Count);
-    var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
-    Console.WriteLine($"[{timestamp}] {randomWords[randomIndex]}");
-    Thread.Sleep(5000);
-}
+var currentWord = randomWords[random.Next(randomWords.Count)];
+var currentTimestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
 
+
+_ = Task.Run(() =>
+{
+    while (true)
+    {
+        currentWord = randomWords[random.Next(randomWords.Count)];
+        currentTimestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        Console.WriteLine($"[{currentTimestamp}] {currentWord}");
+        Thread.Sleep(5000);
+    }
+});
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", ()=> Results.Json(new
+{
+    timestamp = currentTimestamp,
+    randomString = currentWord
+
+}));
+
+app.Run();
--- TheProject/obj/Debug/net10.0/TheProject.AssemblyInfo.cs
+++ TheProject/obj/Debug/net10.0/TheProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc89fbba231acb7fa2e033013381d7b49eb886b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d081f5c4fd6e9d3883f7cf3e9745675e4077c858")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
