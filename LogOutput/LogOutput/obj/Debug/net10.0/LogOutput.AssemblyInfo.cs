7d9c148b72616dde7a1c62d756923291d15b204c
Leo <mangros19@gmail.com>
2026-09-24 09:55:06 +0300

    fixed location for gh actions folder

--- LogOutput/LogOutput/obj/Debug/net10.0/LogOutput.AssemblyInfo.cs
+++ LogOutput/LogOutput/obj/Debug/net10.0/LogOutput.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogOutput")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617641ede86d1c2373977405813feb7bd2be5f99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea320e06e983a8bbc9864d6541a1dee14d8c969")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogOutput")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogOutput")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TheProject/todo-backend/obj/Debug/net10.0/todo-backend.AssemblyInfo.cs
+++ TheProject/todo-backend/obj/Debug/net10.0/todo-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todo-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617641ede86d1c2373977405813feb7bd2be5f99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea320e06e983a8bbc9864d6541a1dee14d8c969")]
 [assembly: System.Reflection.AssemblyProductAttribute("todo-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todo-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
